3263bcdf02886df1b6cb1f23120736eb9852ca52
Sad3dj <deadque@mail.ru>
2023-07-03 17:42:02 +0300

    chnged  4,7,9,12 tasks

--- hw-6.js
+++ hw-6.js
@@ -13,10 +13,14 @@ let numbs = [1, 3, 5, 10, 20];
 numbs=numbs.join(' ');
 console.log(numbs);
 // 4 задание 
-let numbs = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-for (let numb of numbs) {
-for (let element of numb)
-console.log(element)}
+let numbs=[]
+for (let i=0; i<3; i++) {
+numbs[i]=[]
+for ( let j=0;j<3; j++){
+numbs[i] [j]=1
+}
+}
+console.log(numbs)
 // 5 задание
 let numbs = [1, 1, 1]
 numbs.push(2,2,2);
@@ -29,10 +33,16 @@ console.log(numbs);
 // 7 задание 
 let answ=prompt('Угадайте число')
 const numbs = [9, 8, 7, 6, 5]
-if (answ===numbs) {
-alert('Вы угадали');
+let IsFound=false;
+for (let numb of numbs) {
+ if (answ==numb) {
+    IsFound=true;
+ }
+}
+if (IsFound==true) {
+console.log('Верно'); 
 } else {
-  alert('Не угадал');
+console.log('Неверно');
 }
 //  8 задание 
 const word = 'abcdef'; 
@@ -40,21 +50,21 @@ const reverse = word.split('').reverse().join('');
 console.log(reverse);
 // 9 задание 
 const numbs=[[1, 2, 3,],[4, 5, 6]]
-for (let numb of numbs) {
-for (let element of numb)
-console.log(element)}
+const result=[...numbs];
+console.log(result)
 // 10 задание
 
-// 1 задание 
+// 11 задание 
 const numbs = [4,7,6,8,5]
 const squaers=numbs.map(function(num)
 { return num*num
 })
 console.log(squaers);
 // 12 задание 
-getlenghtWords(['слово',' ', 'слог','длинное предложение', 'буква']);
-const getlenghtWords=(arrStr,num)=>
-arrStr.filter(s=>s.length===num);
+function getlenghtWords(arr){
+   const getLengthWords = source => source.map(str => str.length);
+   }
+   console.log(getlenghtWords(['слово',' ', 'слог','длинное предложение', 'буква']));
 // 13 задание
 function filterPositive(array) {
   const even=array.filter(el=>el<=-1);
